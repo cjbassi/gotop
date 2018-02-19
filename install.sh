d8d9b44c87b4a69f8c69fe45f3c5f0a793747634
Caleb Bassi <calebjbassi@gmail.com>
2018-02-19 15:56:35 -0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ VERSION=1.0.0
 arch=$(uname -sm)
 
 case "$arch" in
-    Linux\ *64)   exe=linux_amd64   ;;
+    Linux\ *64)   arch=linux_amd64   ;;
 esac
 
-curl -L https://github.com/cjbassi/gotop/releases/download/$VERSION/gotop-$exe > /usr/bin/gotop
-chmod +x /usr/bin/gotop
+curl -L https://github.com/cjbassi/gotop/releases/download/$VERSION/gotop-$arch.tgz > /tmp/gotop.tgz
+tar xf /tmp/gotop.tgz -C /usr/bin
+rm /tmp/gotop.tgz
 
 update() {
     cur_version=$(gotop -v 2>/dev/null)
