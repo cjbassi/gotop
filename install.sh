bf138746d85259fed6d111dfec42471cfe48aa34
Caleb Bassi <calebjbassi@gmail.com>
2018-02-19 01:59:51 -0800

    Added update

--- install.sh
+++ install.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-VERSION=v1.0
+VERSION=1.0.0
 
 download() {
-    curl -L https://github.com/cjbassi/gotop/releases/download/$VERSION/${1} > /usr/bin/gotop
+    arch=$(uname -sm)
+    case "$arch" in
+        Linux\ *64)   exe=gotop-linux_amd64  ;;
+    esac
+    curl -L https://github.com/cjbassi/gotop/releases/download/$VERSION/$exe > /usr/bin/gotop
     chmod +x /usr/bin/gotop
 }
 
+update() {
+    cur_version=$(gotop -v 2>/dev/null)
+    if [[ $? != 0 ]]; then
+        download
+    fi
+    if (( "${cur_version//.}" < "${VERSION//.}" )); then
+        download
+    fi
+}
+
 uninstall() {
-    rm /usr/bin/gotop
+    rm /usr/bin/gotop 2>/dev/null
 }
 
 for opt in "$@"; do
   case $opt in
     --uninstall)
       uninstall
+      exit 0
+      ;;
+    --update)
+      update
       exit 0
       ;;
     *)
@@ -24,7 +42,4 @@ for opt in "$@"; do
   esac
 done
 
-arch=$(uname -sm)
-case "$arch" in
-  Linux\ *64)   download gotop-linux_amd64  ;;
-esac
+download
