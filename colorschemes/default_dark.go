8ca397498bc9416cf335329f766e8186d25080e9
Caleb Bassi <calebjbassi@gmail.com>
2018-04-09 22:00:27 -0700

    Add default-dark colorscheme

---
+++ colorschemes/default_dark.go
@@ -0,0 +1,23 @@
+package colorschemes
+
+var DefaultDark = Colorscheme{
+	Fg: 235,
+	Bg: -1,
+
+	BorderLabel: 235,
+	BorderLine:  6,
+
+	CPULines: []int{4, 3, 2, 1, 5, 6, 7, 8},
+
+	MainMem: 5,
+	SwapMem: 3,
+
+	ProcCursor: 33,
+
+	Sparkline: 4,
+
+	DiskBar: 252,
+
+	TempLow:  2,
+	TempHigh: 1,
+}
