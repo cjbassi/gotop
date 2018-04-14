7d0abdad08b0fa606f64b325a8a21d7d38e5ac84
Caleb Bassi <calebjbassi@gmail.com>
2018-04-14 12:49:50 -0700

    Fix download script
    Was downloading the wrong binary, since the cases were out of order.

--- download.sh
+++ download.sh
@@ -11,15 +11,16 @@ download() {
 
 arch=$(uname -sm)
 case "$arch" in
+    # order matters
     Darwin\ *64)        download darwin_amd64   ;;
     Darwin\ *86)        download darwin_386     ;;
-    Linux\ *64)         download linux_amd64    ;;
-    Linux\ *86)         download linux_386      ;;
     Linux\ armv5*)      download linux_arm5     ;;
     Linux\ armv6*)      download linux_arm6     ;;
     Linux\ armv7*)      download linux_arm7     ;;
     Linux\ armv8*)      download linux_arm8     ;;
     Linux\ aarch64*)    download linux_arm8     ;;
+    Linux\ *64)         download linux_amd64    ;;
+    Linux\ *86)         download linux_386      ;;
     *)
         echo "No binary found for your system"
         exit 1
