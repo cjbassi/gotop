2b485e00b349890e137e981203f4883827eeeae7
Caleb Bassi <calebjbassi@gmail.com>
2018-05-23 22:48:19 -0700

    Fix building issues
    * can't use goreleaser with darwin builds so we have to manual build
    them ourselves

--- build/build-darwin-cgo.sh
+++ build/build-darwin-cgo.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-xgo --targets="darwin/386,darwin/amd64" $PWD
-mv gotop-darwin-10.6-386 dist/darwin_386/gotop
-mv gotop-darwin-10.6-amd64 dist/darwin_amd64/gotop
+version=$(go run main.go -v)
+
+xgo --targets="darwin/386,darwin/amd64" .
+tar czf gotop_$version_darwin_386.tgz gotop-darwin-10.6-386
+tar czf gotop_$version_darwin_amd64.tgz gotop-darwin-10.6-amd64
