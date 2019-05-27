842c034e7e3637cf09ffa14d60d99dcc4235c376
Matthias Gamsjager <matthias.gamsjager@philips.com>
2019-05-27 10:01:25 +0200

    Refactor memory readings

--- src/widgets/mem_freebsd.go
+++ src/widgets/mem_freebsd.go
@@ -15,23 +15,29 @@ import (
 type SwapInfo struct {
 	Total      uint64
 	Used       uint64
-	Percentage string
+	Percentage float64
 }
 
 func convert(s []string) (SwapInfo, error) {
 	total, err := strconv.ParseUint(s[0], 10, 64)
 	if err != nil {
-		return SwapInfo{}, fmt.Errorf("float converion failed %v", err)
+		return SwapInfo{}, fmt.Errorf("int converion failed %v", err)
 	}
 
 	used, err := strconv.ParseUint(s[1], 10, 64)
 	if err != nil {
+		return SwapInfo{}, fmt.Errorf("int converion failed %v", err)
+	}
+
+	percentage, err := strconv.ParseFloat(s[2], 64)
+	if err != nil {
 		return SwapInfo{}, fmt.Errorf("float converion failed %v", err)
 	}
+
 	return SwapInfo{
 		Total:      total * utils.KB,
 		Used:       used * utils.KB,
-		Percentage: s[2],
+		Percentage: percentage,
 	}, nil
 }
 
@@ -70,10 +76,10 @@ func (self *MemWidget) update() {
 	if err != nil {
 		log.Printf("failed to get swap memory info from gopsutil: %v", err)
 	} else {
-		self.Data["Swap"] = append(self.Data["Swap"], 0)
+		self.Data["Swap"] = append(self.Data["Swap"], swapMemory.Percentage)
 		swapMemoryTotalBytes, swapMemoryTotalMagnitude := utils.ConvertBytes(swapMemory.Total)
 		swapMemoryUsedBytes, swapMemoryUsedMagnitude := utils.ConvertBytes(swapMemory.Used)
-		self.Labels["Swap"] = fmt.Sprintf("%3s   %5.1f%s/%.0f%s",
+		self.Labels["Swap"] = fmt.Sprintf("%3.0f%% %5.1f%s/%.0f%s",
 			swapMemory.Percentage,
 			swapMemoryUsedBytes,
 			swapMemoryUsedMagnitude,
