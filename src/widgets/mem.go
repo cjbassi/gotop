45b5b2dbc0c2fcb6d4973bb9d13a28e23f2d1419
Matthias Gamsjager <matthias.gamsjager@philips.com>
2019-05-28 11:43:29 -0700

    Fix issues with swap memory on FreeBSD

--- src/widgets/mem.go
+++ src/widgets/mem.go
@@ -16,6 +16,38 @@ type MemWidget struct {
 	updateInterval time.Duration
 }
 
+type MemoryInfo struct {
+	Total       uint64
+	Used        uint64
+	UsedPercent float64
+}
+
+func (self *MemWidget) renderMemInfo(line string, memoryInfo MemoryInfo) {
+	self.Data[line] = append(self.Data[line], memoryInfo.UsedPercent)
+	memoryTotalBytes, memoryTotalMagnitude := utils.ConvertBytes(memoryInfo.Total)
+	memoryUsedBytes, memoryUsedMagnitude := utils.ConvertBytes(memoryInfo.Used)
+	self.Labels[line] = fmt.Sprintf("%3.0f%% %5.1f%s/%.0f%s",
+		memoryInfo.UsedPercent,
+		memoryUsedBytes,
+		memoryUsedMagnitude,
+		memoryTotalBytes,
+		memoryTotalMagnitude,
+	)
+}
+
+func (self *MemWidget) updateMainMemory() {
+	mainMemory, err := psMem.VirtualMemory()
+	if err != nil {
+		log.Printf("failed to get main memory info from gopsutil: %v", err)
+	} else {
+		self.renderMemInfo("Main", MemoryInfo{
+			Total:       mainMemory.Total,
+			Used:        mainMemory.Used,
+			UsedPercent: mainMemory.UsedPercent,
+		})
+	}
+}
+
 func NewMemWidget(updateInterval time.Duration, horizontalScale int) *MemWidget {
 	self := &MemWidget{
 		LineGraph:      ui.NewLineGraph(),
@@ -26,49 +58,17 @@ func NewMemWidget(updateInterval time.Duration, horizontalScale int) *MemWidget 
 	self.Data["Main"] = []float64{0}
 	self.Data["Swap"] = []float64{0}
 
-	self.update()
+	self.updateMainMemory()
+	self.updateSwapMemory()
 
 	go func() {
 		for range time.NewTicker(self.updateInterval).C {
 			self.Lock()
-			self.update()
+			self.updateMainMemory()
+			self.updateSwapMemory()
 			self.Unlock()
 		}
 	}()
 
 	return self
-}
-
-func (self *MemWidget) update() {
-	mainMemory, err := psMem.VirtualMemory()
-	if err != nil {
-		log.Printf("failed to get main memory info from gopsutil: %v", err)
-	} else {
-		self.Data["Main"] = append(self.Data["Main"], mainMemory.UsedPercent)
-		mainMemoryTotalBytes, mainMemoryTotalMagnitude := utils.ConvertBytes(mainMemory.Total)
-		mainMemoryUsedBytes, mainMemoryUsedMagnitude := utils.ConvertBytes(mainMemory.Used)
-		self.Labels["Main"] = fmt.Sprintf("%3.0f%% %5.1f%s/%.0f%s",
-			mainMemory.UsedPercent,
-			mainMemoryUsedBytes,
-			mainMemoryUsedMagnitude,
-			mainMemoryTotalBytes,
-			mainMemoryTotalMagnitude,
-		)
-	}
-
-	swapMemory, err := psMem.SwapMemory()
-	if err != nil {
-		log.Printf("failed to get swap memory info from gopsutil: %v", err)
-	} else {
-		self.Data["Swap"] = append(self.Data["Swap"], swapMemory.UsedPercent)
-		swapMemoryTotalBytes, swapMemoryTotalMagnitude := utils.ConvertBytes(swapMemory.Total)
-		swapMemoryUsedBytes, swapMemoryUsedMagnitude := utils.ConvertBytes(swapMemory.Used)
-		self.Labels["Swap"] = fmt.Sprintf("%3.0f%% %5.1f%s/%.0f%s",
-			swapMemory.UsedPercent,
-			swapMemoryUsedBytes,
-			swapMemoryUsedMagnitude,
-			swapMemoryTotalBytes,
-			swapMemoryTotalMagnitude,
-		)
-	}
 }
