246ebfbff2c87f66218c42ea9490214006f2efc8
Brian Mattern <rephorm@rephorm.com>
2019-06-06 11:08:05 -0700

    Remove unused duplicate definition.

--- src/termui/entry.go
+++ src/termui/entry.go
@@ -71,25 +71,6 @@ func (self *Entry) HandleEvent(e Event) bool {
 	return true
 }
 
-func TruncateFront(s string, w int, prefix string) string {
-	if rw.StringWidth(s) < w {
-		return s
-	}
-	r := []rune(s)
-	pw := rw.StringWidth(prefix)
-	w -= pw
-	width := 0
-	i := len(r) - 1
-	for ; i >= 0 && width <= w; i-- {
-		cw := rw.RuneWidth(r[i])
-		width += cw
-		if width > w {
-			break
-		}
-	}
-	return prefix + string(r[i+1:len(r)])
-}
-
 func (self *Entry) Draw(buf *Buffer) {
 	if self.Value == "" && !self.editing && !self.ShowWhenEmpty {
 		return
