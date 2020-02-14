eb27f7413185494d81f0293f55b9064179adb278
Sean E. Russell <ser@ser1.net>
2020-02-14 06:44:25 -0600

    Add merge todos

--- config.go
+++ config.go
@@ -9,6 +9,17 @@ import (
 )
 
 // TODO: Cross-compiling for darwin, openbsd requiring native procs & temps
+// TODO: Merge #184 or #177 degree symbol (BartWillems:master, fleaz:master)
+// TODO: Merge #169 % option for network use (jrswab:networkPercentage)
+// TODO: Merge #167 configuration file (jrswab:configFile111)
+// TODO: Merge #166 Nord color scheme (jrswab:nordColorScheme)
+// TODO: Merge #165 README updates (theverything:add-missing-option-to-readme)
+// TODO: Merge #157 FreeBSD fixes & Nvidia GPU support (kraust:master)
+// TODO: Merge #156 Added temperatures for NVidia GPUs (azak-azkaran:master)
+// TODO: Merge #152 (implements #148 & #149) (mattLLVW:feature/network_interface_list)
+// TODO: Merge #147 filtering subprocesses by substring (rephorm:filter)
+// TODO: Merge #140 color-related fix (Tazer:master)
+// TODO: Merge #135 linux console font (cmatsuoka:console-font)
 type Config struct {
 	ConfigDir string
 	LogDir    string
