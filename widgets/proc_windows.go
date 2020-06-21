81424fb000eaf1b6cd8c1d5bb4d476cb4b3b32db
Sean E. Russell <ser@ser1.net>
2020-06-21 11:49:04 -0500

    Fix windows compile errors.

--- widgets/proc_windows.go
+++ widgets/proc_windows.go
@@ -3,12 +3,13 @@ package widgets
 import (
 	"fmt"
 	"log"
+	"strconv"
 
-	psProc "github.com/shirou/gopsutil/process"
+	"github.com/shirou/gopsutil/process"
 )
 
 func getProcs() ([]Proc, error) {
-	psProcs, err := psProc.Processes()
+	psProcs, err := process.Processes()
 	if err != nil {
 		return nil, fmt.Errorf(tr.Value("widget.proc.err.gopsutil", err.Error()))
 	}
@@ -18,15 +19,24 @@ func getProcs() ([]Proc, error) {
 		pid := psProc.Pid
 		command, err := psProc.Name()
 		if err != nil {
-			log.Println(tr.Value("widget.proc.err.getcmd", err, psProc, i, pid))
+			sps := fmt.Sprintf("%v", psProc)
+			si := strconv.Itoa(i)
+			spid := fmt.Sprintf("%d", pid)
+			log.Println(tr.Value("widget.proc.err.getcmd", err.Error(), sps, si, spid))
 		}
 		cpu, err := psProc.CPUPercent()
 		if err != nil {
-			log.Println(tr.Value("widget.proc.err.cpupercent", err, psProc, i, pid))
+			sps := fmt.Sprintf("%v", psProc)
+			si := strconv.Itoa(i)
+			spid := fmt.Sprintf("%d", pid)
+			log.Println(tr.Value("widget.proc.err.cpupercent", err.Error(), sps, si, spid))
 		}
 		mem, err := psProc.MemoryPercent()
 		if err != nil {
-			log.Println(tr.Value("widget.proc.err.mempercent", err, psProc, i, pid))
+			sps := fmt.Sprintf("%v", psProc)
+			si := strconv.Itoa(i)
+			spid := fmt.Sprintf("%d", pid)
+			log.Println(tr.Value("widget.proc.err.mempercent", err.Error(), sps, si, spid))
 		}
 
 		procs[i] = Proc{
